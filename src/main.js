0982dc2ab8d51beba6827133561c912df09841be
Paul Jomy <pauljomy@users.noreply.github.com>
2026-02-13 15:14:56 +0530

    share button on desktop and tablet completed

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import twitterIcon from "./assets/img/icon-twitter.svg";
 import pinterestIcon from "./assets/img/icon-pinterest.svg";
 import shareIcon from "./assets/img/icon-share.svg";
 
-const shareBtn = document.getElementById("share-btn");
 const footer = document.getElementById("footer");
+const tabletIcons = document.getElementById("tablet-share-icons");
+const shareBtn = document.getElementById("share-btn");
 const originalFooter = footer.innerHTML;
 let isShareOpen = false;
 
@@ -13,14 +14,27 @@ document.addEventListener("click", (e) => {
   if (!btn) return;
 
   const isMobile = window.matchMedia("(max-width:767px)").matches;
-  if (!isMobile) return;
+  const isTablet = window.matchMedia("(min-width:768px)").matches;
+  if (isMobile) {
+    isShareOpen = !isShareOpen;
 
-  isShareOpen = !isShareOpen;
+    if (isShareOpen) {
+      footer.innerHTML = shareIcons;
+    } else {
+      footer.innerHTML = originalFooter;
+    }
+  } else if (isTablet) {
+    isShareOpen = !isShareOpen;
 
-  if (isShareOpen) {
-    footer.innerHTML = shareIcons;
+    if (isShareOpen) {
+      tabletIcons.innerHTML = tabletShareIcons;
+      shareBtn.style.backgroundColor = "#6E8098";
+    } else {
+      tabletIcons.innerHTML = "";
+      shareBtn.style.backgroundColor = "#ECF2F8";
+    }
   } else {
-    footer.innerHTML = originalFooter;
+    return;
   }
 
   console.log("Button clicked");
@@ -58,3 +72,31 @@ const shareIcons = `
       />
     </div>
   </div>`;
+
+const tabletShareIcons = `    <div
+  id="share-btn-close"
+  class="md:shadow-xl md:bottom-14 md:-right-18 md:w-62 md:h-12 md:rounded-xl md:absolute md:px-5 h-20 bg-[#48556A] rounded-b-xl flex items-center justify-center px-5"
+>
+  <div class="flex gap-5 items-center">
+    <span class="text-[0.8125rem] text-[#9DAEC2]">SHARE</span>
+    <span
+      ><img
+        src="${facebookIcon}"
+        alt="An icon of facebook"
+    /></span>
+    <span
+      ><img
+        src="${twitterIcon}"
+        alt="An icon of twitter"
+    /></span>
+    <span
+      ><img
+        src="${pinterestIcon}"
+        alt="An icon of pinterest"
+    /></span>
+  </div>
+
+  <div
+    class="shadow-xl md:absolute md:-bottom-2.5 md:right-28 h-5 w-5 bg-dark-graish-blue rotate-45"
+  ></div>
+</div>`;
